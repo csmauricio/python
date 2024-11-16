b5192be706d58bbc241472ab791341edfdacc064
Mauricio Caio Simoes <m_c_simoes@yahoo.com.br>
2024-11-16 15:29:57 -0300

    Variáveis numéricas

--- variaveis_e_tipos_de_dados_numero.py
+++ variaveis_e_tipos_de_dados_numero.py
@@ -1,1 +1,35 @@
 #Variáveis Numero
+
+#Operações
+valor = 2 + 3
+print(valor)
+
+valor = 3 - 2
+print(valor)
+
+valor = 2 * 3
+print(valor)
+
+valor = 2 ** 3
+print(valor)
+
+#no python2 a divisão dá sempre em inteiro, ao menos que um dos numeros seja decimal
+valor = 3 / 2
+print(valor)
+
+valor = 2 + 3 * 4
+print(valor)
+
+valor = (2 + 3) * 4
+print(valor)
+
+#valores decimais
+valor = 0.2 + 0.1
+print(valor)
+
+print(2 + 3)
+
+#Converter em texto
+idade = 23
+message = "Feliz " + str(idade) + "° aniversário!"
+print(message)
